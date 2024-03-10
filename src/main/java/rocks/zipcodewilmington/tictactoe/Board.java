1d5c4af381a14076e451f32f78ee56610bea2b5f
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-03-10 19:26:51 -0400

    started x

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -3,24 +3,39 @@ package rocks.zipcodewilmington.tictactoe;
 /**
  * @author leon on 6/22/18.
  */
+
 public class Board {
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
-    }
+        for (int i = 0; i < matrix.length; i++ ) {
+            if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2] && (matrix[i][0] == 'X')) {
+                return true;
+            }
+        }
+        for (int j = 0; j < matrix.length; j++) {
+            if (matrix[0][j] == matrix[1][j] && matrix[1][j] == matrix[2][j] && (matrix[0][j] == 'X')) {
+                return true;
+            }
+        }
+            return null;
+}
 
-    public Boolean isInFavorOfO() {
-        return null;
-    }
+        public Boolean isInFavorOfO () {
+            return null;
+        }
 
-    public Boolean isTie() {
-        return null;
-    }
+        public Boolean isTie () {
+            return null;
+        }
 
-    public String getWinner() {
-        return null;
-    }
+        public String getWinner () {
+            return null;
+        }
 
 }
+
